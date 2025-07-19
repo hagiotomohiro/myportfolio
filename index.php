80f10551addbd538551162830f82fefe472441c4
hagiotomohiro <164848969+hagiotomohiro@users.noreply.github.com>
2025-07-20 00:13:06 +0900

    追加：MVのマークアップ

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php include 'header.php'; ?>
 
-<main></main>
+<!-- Main visual start -->
+<div id="top" class="main-visual">
+    <h1 class="main-visual__title">
+        Tomohiro Hagio's Portfolio
+    </h1>
+    <div class="main-visual__mask"></div>
+</div>
+<!-- /.main-visual -->
+<!-- Mainvisual end -->
+
+<main>
+
+</main>
 
 <?php include 'footer.php'; ?>
