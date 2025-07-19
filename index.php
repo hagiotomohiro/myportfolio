af935c1b197f66f5ea3035f64b82fc61079e5eaa
hagiotomohiro <164848969+hagiotomohiro@users.noreply.github.com>
2025-07-20 00:22:36 +0900

    追加：Worksセクションのマークアップ

--- index.php
+++ index.php
@@ -10,7 +10,70 @@
 <!-- /.main-visual -->
 <!-- Mainvisual end -->
 
-<main>
+<main class="container">
+    <!-- WORKS start -->
+    <section id="works" class="works">
+        <div class="section-title open">
+            <span class="section-title__main--en">WORKS</span>
+            <h2 class="section-title__sub--ja">制作実績</h2>
+        </div>
+        <!-- /.section-title -->
+        <div class="works__column">
+            <div class="works-card fadein">
+                <a href="page/sample-1.html">
+                    <img class="works-card__image" src="assets/img/sample1.webp" alt="小さなパスタ屋Tino ホームページ">
+                    <h3 class="works-card__title">小さなパスタ屋Tino</h3>
+                </a>
+                <ul class="works-card__list">
+                    <li class="works-card__item">実案件</li>
+                    <li class="works-card__item">Webサイト</li>
+                    <li class="works-card__item">デザイン</li>
+                    <li class="works-card__item">コーディング</li>
+                </ul>
+            </div>
+            <!-- /.works-card -->
+            <div class="works-card fadein">
+                <a href="page/sample-2.html">
+                    <img class="works-card__image" src="assets/img/sample2.webp" alt="株式会社アリオン ホームページ">
+                    <h3 class="works-card__title">株式会社アリオン</h3>
+                </a>
+                <ul class="works-card__list">
+                    <li class="works-card__item">実案件</li>
+                    <li class="works-card__item">Webサイト</li>
+                    <li class="works-card__item">その他</li>
+                    <li class="works-card__item">コーディング</li>
+                    <li class="works-card__item">WordPress</li>
+                </ul>
+            </div>
+            <!-- /.works-card -->
+            <div class="works-card fadein">
+                <a href="page/sample-3.html">
+                    <img class="works-card__image" src="assets/img/sample3.webp" alt="クリエイティブ・ギルド　キユリアス">
+                    <h3 class="works-card__title">クリエイティブ・ギルド　キユリアス</h3>
+                </a>
+                <ul class="works-card__list">
+                    <li class="works-card__item">実案件</li>
+                    <li class="works-card__item">Webサイト</li>
+                    <li class="works-card__item">コーディング</li>
+                </ul>
+            </div>
+            <!-- /.works-card -->
+            <div class="works-card fadein">
+                <a href="page/sample-4.html">
+                    <img class="works-card__image" src="assets/img/sample4.webp" alt="弓削島制作舎　福山Region　ランディングページ">
+                    <h3 class="works-card__title">福山Region　ランディングページ</h3>
+                </a>
+                <ul class="works-card__list">
+                    <li class="works-card__item">実案件</li>
+                    <li class="works-card__item">ランディングページ</li>
+                    <li class="works-card__item">コーディング</li>
+                </ul>
+            </div>
+            <!-- /.works-card -->
+        </div>
+        <!-- /.works__column -->
+    </section>
+    <!-- WORKS end -->
 
 </main>
 
