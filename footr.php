28f255d8be96e18bdb468abf8cc0403404ee3e47
hagiotomohiro <164848969+hagiotomohiro@users.noreply.github.com>
2025-07-20 00:05:13 +0900

    追加: ヘッダー・フッター共通パーツの追加とスタイル調整
    - header.php、footr.phpの新規追加
    - _common.scssにh4, imgのスタイルを追加・調整
    - index.phpを共通パーツ対応に修

---
+++ footr.php
@@ -0,0 +1,9 @@
+<!-- footer start -->
+<footer class="footer open">
+    <p class="footer__copyright">&copy; 2024 Tomohiro hagio</p>
+</footer>
+<!-- footer end -->
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="description" content="萩尾智弘のポートフォリオサイト">
+    <title>Hagio Tomohiro's Portfolio</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Twitterカード -->
+    <meta name="twitter:card" content="summary">
+    <meta property="og:url" content="https://pilina-cs.com/">
+    <meta property="og:title" content="Hagio Tomohiro's Portfolio">
+    <meta property="og:description" content="萩尾智弘のポートフォリオサイト">
+    <meta property="og:image" content="https://pilina-cs.com/assets/img/twitter_card.png">
+    <!-- Googl font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <!-- header start -->
+    <header>
+        <!-- ハンバーガーメニュー -->
+        <div class="hamburger">
+            <span class="hamburger__line"></span>
+            <span class="hamburger__line"></span>
+            <span class="hamburger__text">MENU</span>
+        </div>
+        <!-- /.hamburger -->
+        <nav class="gnav">
+            <ul class="gnav__list">
+                <li class="gnav__item">
+                    <a class="gnav__link" href="#top">HOME</a>
+                </li>
+                <li class="gnav__item">
+                    <a class="gnav__link" href="#works">WORKS</a>
+                </li>
+                <li class="gnav__item">
+                    <a class="gnav__link" href="#skill">SKILL</a>
+                </li>
+                <li class="gnav__item">
+                    <a class="gnav__link" href="#about">ABOUT</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <!-- header end -->
