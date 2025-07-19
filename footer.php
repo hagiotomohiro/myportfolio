428e372d9dd92a30efb46628ff14fc472a32071a
hagiotomohiro <164848969+hagiotomohiro@users.noreply.github.com>
2025-07-20 01:02:13 +0900

    修正：ファイル名の誤字修正

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
 </footer>
 <!-- footer end -->
 <script src="https://rawgit.com/kimmobrunfeldt/progressbar.js/master/dist/progressbar.js" defer></script>
-<script src="assets/js/particles.js" defer></script>
-<script src="assets/js/app.js" defer></script>
-<script src="assets/js/main.js" defer></script>
+<script src="./assets/js/particles.js" defer></script>
+<script src="./assets/js/app.js" defer></script>
+<script src="./assets/js/main.js" defer></script>
 </body>
 
 </html>
