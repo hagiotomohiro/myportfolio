24534fd6914b54ba79fb235b7488810fd1373108
hagiotomohiro <164848969+hagiotomohiro@users.noreply.github.com>
2025-07-20 01:03:58 +0900

    修正：スクロールアニメーションのフェードイン修正

--- assets/js/main.js
+++ assets/js/main.js
@@ -154,3 +154,12 @@ const animateFade = (entries, obs) => {
     }
   });
 };
+
+// 監視設定
+const fadeObserver = new IntersectionObserver(animateFade);
+
+// .fadeinを監視するよう指示
+const fadeElements = document.querySelectorAll('.fadein');
+fadeElements.forEach((fadeElement) => {
+  fadeObserver.observe(fadeElement);
+});
