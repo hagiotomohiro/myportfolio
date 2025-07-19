12a4024cb94e06a0eb845a31bd551e54495a7a1b
hagiotomohiro <164848969+hagiotomohiro@users.noreply.github.com>
2025-07-20 01:46:10 +0900

    追加：残りの下層ページのマークアップ

---
+++ pages/sample-4.php
@@ -0,0 +1,66 @@
+<?php include '../header.php'; ?>
+
+<div class="top">
+    <h1 class="top__title">制作実績</h1>
+    <!-- パンくずリスト -->
+    <nav aria-label="Breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb__item">
+                <a href="../index.php" class="breadcrumb__link">ホーム</a>
+            </li>
+            <li class="breadcrumb__item">
+                制作実績 [ No.4 ]
+            </li>
+        </ol>
+    </nav>
+</div>
+<!-- /.top -->
+
+<main class="container">
+    <section class="description fadein">
+        <img class="description__image" src="../assets/images/sample4.webp" alt="福山Region　ランディングページ">
+        <div class="description__caption">
+            <h2 class="description__title">福山Region　ランディングページ</h2>
+            <ul class="description__list works-card__list">
+                <li class="works-card__item">実案件</li>
+                <li class="works-card__item">ランディングページ</li>
+                <li class="works-card__item">コーディング</li>
+            </ul>
+            <a class="description__link button" href="https://bingo.9re8.xyz/" target="_blank">
+                サイトはこちら
+            </a>
+        </div>
+        <!-- /.description__caption -->
+    </section>
+    <section class="detail fadein">
+        <h4 class="detail__heading">
+            web制作会社「弓削島制作舎」の福山エリアのキャンペーンのランディングページ制作のコーディングを担当しました。
+        </h4>
+        <dl class="detail__contents">
+            <dt class="detail__title">依頼目的</dt>
+            <dd class="detail__text">店舗移転に伴い店舗の認知向上、集客向上</dd>
+            <dt class="detail__title">コンセプト</dt>
+            <dd class="detail__text">幅広いターゲット層に対応しシンプルかつ優し気な店舗の雰囲気を意識したデザイン</dd>
+            <dt class="detail__title">制作ポイント</dt>
+            <dd class="detail__text">
+                問い合わせフォームをPHPで実装しました。記入漏れ等のエラー表示、セキュリティ対策、問い合わせ者への自動返信を搭載しています。< </dd>
+            <dt class="detail__title">制作期間</dt>
+            <dd class="detail__text">
+                <ul class="point">
+                    <li class="point__item">要件整理：2日</li>
+                    <li class="point__item">コーディング：40日間</li>
+                </ul>
+            </dd>
+        </dl>
+    </section>
+    <!-- pagination -->
+    <div class="pagination pagination--end fadein">
+        <a class="pagination__contents pagination-reverse__contents" href="sample-3.php">
+            <span class="pagination__text">No.3</span>
+            <img class="pagination__image" src="../assets/images/sample3.webp">
+        </a>
+    </div>
+    <!-- /.pagination -->
+</main>
+
+<?php include '../footer.php'; ?>
